d80800c3cd132b3f68622d897b23178a8252ca0e
Peter Cotton <peter.cotton@microprediction.com>
2021-01-04 13:37:26 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="timemachines",
+    version="0.0.1",
+    description="Really simple pure function time series prediction models",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/microprediction/pointy",
+    author="microprediction",
+    author_email="pcotton@intechinvestments.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["timemachines"],
+    test_suite='pytest',
+    tests_require=['pytest'],
+    include_package_data=True,
+    install_requires=["pathlib"],
+    entry_points={
+        "console_scripts": [
+            "pointy=pointy.__main__:main",
+        ]
+    },
+)
