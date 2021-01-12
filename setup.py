c6248e663fa7030471e3ecec7a1c12a9fcb4f362
Peter Cotton <peter.cotton@microprediction.com>
2021-01-12 14:30:01 -0500

    deps deps

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     test_suite='pytest',
     tests_require=['pytest'],
     include_package_data=True,
-    install_requires=["pathlib","numpy>=1.16.5","microconventions>0.5.0","sklearn","divinity","pmdarima","hyperopt","scipy",
+    install_requires=["pathlib","numpy>=1.16.5","importlib-metadata>=1.7.0","microconventions>0.5.0","sklearn","divinity","pmdarima","hyperopt","scipy",
                       "poap","pySOT","funcy","pydlm","optuna","deap","ax-platform","sigopt","statsmodels","tdigest","platypus-opt"],
     entry_points={
         "console_scripts": [
