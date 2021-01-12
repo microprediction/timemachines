d7a2f1e408f1c2e61a1a2f0af7847a854d20e4d6
Peter Cotton <peter.cotton@microprediction.com>
2021-01-12 16:22:36 -0500

    shorter test

--- tests/test_pmd.py
+++ tests/test_pmd.py
@@ -3,8 +3,8 @@ from timemachines.evaluation import quick_brown_fox, lazy_dog
 
 
 def test_pmd_auto_univariate():
-    err = quick_brown_fox(f=pmd_auto, n=500)
+    err = quick_brown_fox(f=pmd_auto, n=150)
 
 
 def test_pmd_auto_exogenous():
-    err = lazy_dog(f=pmd_auto,n=500)
+    err = lazy_dog(f=pmd_auto,n=150)
