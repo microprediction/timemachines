3a23bfbe34f839389d66d55e051195443afde96c
Peter Cotton <peter.cotton@microprediction.com>
2021-01-12 14:26:36 -0500

    numpy ver bump

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     test_suite='pytest',
     tests_require=['pytest'],
     include_package_data=True,
-    install_requires=["pathlib","microconventions>0.5.0","sklearn","divinity","pmdarima","hyperopt","scipy",
+    install_requires=["pathlib","numpy>=1.16.5","microconventions>0.5.0","sklearn","divinity","pmdarima","hyperopt","scipy",
                       "poap","pySOT","funcy","pydlm","optuna","deap","ax-platform","sigopt","statsmodels","tdigest"],
     entry_points={
         "console_scripts": [
