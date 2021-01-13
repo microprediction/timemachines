5aa97ced604120270399efb7f764c684c36bd417
Peter Cotton <peter.cotton@microprediction.com>
2021-01-13 16:00:09 -0500

    deps  Please enter the commit message for your changes. Lines starting

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="timemachines",
-    version="0.0.14",
+    version="0.0.15",
     description="Time series models represented as pure functions with SKATER convention.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -26,7 +26,8 @@ setup(
     include_package_data=True,
     install_requires=["pathlib","numpy>=1.16.5","importlib-metadata>=1.7.0","microconventions>0.5.0","sklearn","divinity","pmdarima","hyperopt","scipy",
                       "poap","pySOT","funcy","pydlm","optuna","deap","ax-platform","sigopt","statsmodels","tdigest","platypus-opt",
-                      "pymoo","pyflux"],
+                      "pymoo"],
+    dependency_links=["git+git://github.com/RJT1990/pyflux.git"],
     entry_points={
         "console_scripts": [
             "timemachines=timemachines.__main__:main",
