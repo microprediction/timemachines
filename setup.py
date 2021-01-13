c24a1b77f6cbcfaca08ee52eeb8b828cae02e5db
Peter Cotton <peter.cotton@microprediction.com>
2021-01-13 16:33:53 -0500

    packages/sub

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="timemachines",
-    version="0.0.18",
+    version="0.0.19",
     description="Time series models represented as pure functions with SKATER convention.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -20,7 +20,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=["timemachines"],
+    packages=["timemachines","timemachines.optimizers","timemachines.skaters"],
     test_suite='pytest',
     tests_require=['pytest'],
     include_package_data=True,
