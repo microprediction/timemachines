d5256bd340137f340e2cab1d41619ab578926b14
Peter Cotton <peter.cotton@microprediction.com>
2021-01-20 18:24:47 -0500

    simplified stochastic tests

--- timemachines/stochastictests/allstochastictests.py
+++ timemachines/stochastictests/allstochastictests.py
@@ -1,5 +1,4 @@
-from timemachines.stochastictests.optimizedlm import optimize_dlm
 from timemachines.stochastictests.optimizerandomskater import optimize_random_skater
-from timemachines.stochastictests.rundlm import run_dlm
 
-STOCHASTIC_TESTS = [ run_dlm, optimize_dlm, optimize_random_skater ]
+
+STOCHASTIC_TESTS = [ optimize_random_skater ]
