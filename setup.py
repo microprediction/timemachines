d37c7f7a18cd3232a11ac6fbfbbb816fafa4a31a
Peter Cotton <peter.cotton@microprediction.com>
2021-02-05 14:57:42 -0500

    modified fbprophet dependency as pypi seems broken

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="timemachines",
-    version="0.2.0",
+    version="0.2.1",
     description="Bringing together popular time series packages, and popular optmization packages for hyper-param selection.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -24,9 +24,11 @@ setup(
     test_suite='pytest',
     tests_require=['pytest'],
     include_package_data=True,
-    install_requires=["wheel","pathlib","numpy>=1.16.5","importlib-metadata>=1.7.0","microconventions>0.5.0","getjson","sklearn","divinity","pmdarima","hyperopt","scipy",
+    install_requires=["wheel","pathlib","numpy>=1.16.5","importlib-metadata>=1.7.0","microconventions>0.5.0","getjson","sklearn",
+                      "divinity","pmdarima","hyperopt","scipy",
                       "poap","pySOT","funcy","pydlm","optuna","deap","ax-platform","sigopt","statsmodels","tdigest","platypus-opt",
-                      "pymoo","ratings","nevergrad","swarmlib","momentum","fbprophet","pandas"],
+                      "pymoo","ratings","nevergrad","swarmlib","momentum","pandas",
+                      "-e git://github.com/facebook/prophet.git#egg=prophet"],
     entry_points={
         "console_scripts": [
             "timemachines=timemachines.__main__:main",
--- timemachines/__init__.py
+++ timemachines/__init__.py
@@ -1,2 +1,3 @@
 from timemachines.optimizers.alloptimizers import OPTIMIZERS
 from timemachines.skaters.allskaters import SKATERS
+from timemachines.optimizers.objectives import OBJECTIVES
