ff3650244f8c750508dfa9241b7a7d49ab85dd41
Home <home0@homes-mbp.lan>
2021-02-28 10:30:35 -0500

    made defaults same as notebook https://github.com/microprediction/timeseries-notebooks/blob/main/neuralprophet_hello.ipynb

--- timemachines/skaters/nproph/nprophdefaultparams.py
+++ timemachines/skaters/nproph/nprophdefaultparams.py
@@ -1,25 +1,15 @@
 
-NPROPH_CRITERIA = ['aic', 'aicc', 'bic', 'hqic']
-
-
 def nproph_params(method=None):
     """ Default hyper-params """
     # https://alkaline-ml.com/pmdarima/modules/generated/pmdarima.arima.auto_arima.html
     s = dict(
-        start_p=1,
-        start_q=1,
-        start_P=1,
-        start_Q=1,
-        max_p=10, 
-        max_q=10,
-        max_P=24,
-        max_Q=24,
-        seasonal=False,
-        stepwise=True, 
-        suppress_warnings=True,
-        D=None, max_D=3,
-        error_action='ignore',
-        information_criterion=NPROPH_CRITERIA[0]
+        nlags=4,
+        changepoints_range=0.95,
+        n_changepoints=30,
+        weekly_seasonality=False,
+        batch_size=64,
+        epochs=10,
+        learning_rate=1.0
     )
     if method == '0':
         # Add more cases here
