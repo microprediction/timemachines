fb6bfd5b0b6b0c0c520fb9b087b16d60c3dbc3e6
Peter Cotton <peter.cotton@microprediction.com>
2021-07-20 00:50:06 -0400

    ver

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="timemachines",
-    version="0.10.3",
+    version="0.10.4",
     description="Evaluation and standardization of popular time series packages",
     long_description=README,
     long_description_content_type="text/markdown",
