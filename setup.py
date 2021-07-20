58fc3b5170e98aacc0596f668df079111d667fed
Peter Cotton <peter.cotton@microprediction.com>
2021-07-20 13:33:02 -0400

    missing init

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="timemachines",
-    version="0.11.1",
+    version="0.11.2",
     description="Evaluation and standardization of popular time series packages",
     long_description=README,
     long_description_content_type="text/markdown",
