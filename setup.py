ad2fb1954f61d243a80f982cb63235558b4f81cc
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-07-27 21:10:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="timemachines",
-    version="0.11.21",
+    version="0.11.22",
     description="Evaluation and standardization of popular time series packages",
     long_description=README,
     long_description_content_type="text/markdown",
