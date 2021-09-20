278025615f29b56db6321594dee448af9bb1dcd2
Ian Klasky <ianklasky@Ians-MacBook-Pro.local>
2021-09-20 00:45:21 -0400

    adding NBEATS to the fray, and fixing what I believe to be a typo in the Theta file naming

--- timemachines/skaters/drts/dartswrappers.py
+++ timemachines/skaters/drts/dartswrappers.py
@@ -5,7 +5,7 @@ if using_darts:
     import pandas as pd
     from typing import List
     from darts import TimeSeries
-    from darts.models import FFT,Prophet,ARIMA,AutoARIMA,ExponentialSmoothing,Theta,FourTheta,TransformerModel
+    from darts.models import FFT,Prophet,ARIMA,AutoARIMA,ExponentialSmoothing,Theta,FourTheta,TransformerModel,NBEATSModel
     from darts.utils.utils import SeasonalityMode
 
     def darts_FFT_iskater(y: [[float]], k: int, a: List = None, t: List = None, e=None, deseasonalize=False):
@@ -196,6 +196,34 @@ if using_darts:
         train, val = series[:-k], series[-k:]
 
         model = FourTheta(season_mode = SeasonalityMode.ADDITIVE)
+        model.fit(train)
+        prediction = model.predict(len(val))
+
+        x = prediction.pd_dataframe()['y'].values.tolist()
+        x_std = [1]*k
+
+        return x, x_std
+
+    def darts_NBEATS_iskater(y: [[float]], k: int, a: List = None, t: List = None, e=None, deseasonalize=False):
+        """
+            Calls Darts' NBEATS forecasting model, but ignores t if supplied.
+        """
+        if a:
+            assert len(a) == len(y) + k
+        if np.isscalar(y[0]):
+            y0s = [ yt for yt in y]
+        else:
+            y0s = [ yt[0] for yt in y ]
+
+        y0s.extend([0]*k)
+        idx = pd.date_range(end='01/01/2000', periods=len(y0s), freq="H")
+        df = pd.DataFrame(y0s, index=idx).reset_index().rename(columns={'index':'ts',0:'y'})
+        df.head()
+
+        series = TimeSeries.from_dataframe(df, 'ts', 'y')
+        train, val = series[:-k], series[-k:]
+
+        model = NBEATSModel(input_chunk_length = 2 * k, output_chunk_length = k)
         model.fit(train)
         prediction = model.predict(len(val))
 
