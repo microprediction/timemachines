bfd53f6981e3862e09b7afa3f498e52573834272
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-10-05 22:21:24 -0400

    turned tbats back on

--- timemachines/skaters/bats/batsinclusion.py
+++ timemachines/skaters/bats/batsinclusion.py
@@ -4,6 +4,5 @@ try:
     using_bats = True
 except ImportError:
     using_bats = False
-
-using_bats = False  # pending issue possibily similar to sklearn similar to https://github.com/alkaline-ml/pmdarima/pull/455
+# pending issue possibily similar to sklearn similar to https://github.com/alkaline-ml/pmdarima/pull/455
 
