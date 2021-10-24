dc8cbecf56d08b20d44a1e3eac9a1dabcae94afa
Peter Cotton <peter.cotton@microprediction.com>
2021-10-24 18:44:10 -0400

    always

--- timemachines/skatertools/comparison/skaterelo.py
+++ timemachines/skatertools/comparison/skaterelo.py
@@ -81,7 +81,9 @@ def skater_elo_multi_update(elo: dict, k, evaluator=None, n_burn=400, tol=0.01, 
     if always_skaters is not None:
         for always_skater in always_skaters:
             if always_skater is not None and (always_skater not in chosen) and (always_skater in elo['name']):
-                chosen.append(elo['name'].index(always_skater))
+                c = elo['name'].index(always_skater)
+                if c not in chosen:
+                    chosen.append(c)
 
     fs = list()
     chosen_and_imported = list()
