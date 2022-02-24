2d3c55ee7e603340c8ee0db9f86384e40cfe0075
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-02-23 23:11:28 -0500

    Update merlioninclusion.py

--- timemachines/skaters/mrln/merlioninclusion.py
+++ timemachines/skaters/mrln/merlioninclusion.py
@@ -4,6 +4,12 @@ try:
     using_merlion = True
 except ImportError:
     using_merlion = False
+    
+try:
+    from merlion.models.forecast.prophet import Prophet, ProphetConfig
+    using_merlion_prophet = True
+except ImportError:
+    using_merlion_prophet = False
 
 if __name__=='__main__':
     print(using_merlion)
