d52eb44a0dac7b108600c9b6498bdf3ea25a9200
Peter Cotton <peter.cotton@microprediction.com>
2022-10-10 16:55:39 -0400

    example

---
+++ examples/basic_usage/run_statsforecast_skater_with_hypocratic_chasing.py
@@ -0,0 +1,18 @@
+try:
+    from timemachines.skaters.sk.sfautoarimahypocratic import sf_autoarima_hypocratic as f
+except ImportError:
+    print('pip install timemachines')
+    print('pip install sktime')
+    print('pip install statsmodels')
+
+import numpy as np
+
+# Example of running Nixtla statsforecast AutoARIMA sequentially over 100 data points
+
+if __name__=='__main__':
+    y = np.cumsum(np.random.randn(100))
+    s = {}
+    x = list()
+    for yi in y:
+        xi, x_std, s = f(y=yi, s=s, k=3, e=1000)
+        x.append(xi)
