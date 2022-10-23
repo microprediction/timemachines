0d7b2d2460e4722bea84f3634139da363e26f546
Peter Cotton <peter@Peters-MacBook-Air.local>
2022-10-23 00:18:10 -0400

    fix

--- timemachines/skaters/sk/sfautoarimawiggly.py
+++ timemachines/skaters/sk/sfautoarimawiggly.py
@@ -10,7 +10,7 @@ if using_statsforecast:
     def sf_autoarima_wiggly_factory(y: Y_TYPE, s: dict, k: int,
                                     a: A_TYPE = None, t: T_TYPE = None,
                                     e: E_TYPE = None, m=5, d=0.1, combiner=None):
-        return wiggler(y=y, s=s, k=k, a=a, t=t, e=e, r=None, f=sf_autoarima, m=m, d=d, combiner=None)
+        return wiggler(y=y, s=s, k=k, a=a, t=t, e=e, r=None, f=sf_autoarima, m=m, d=d, combiner=combiner)
 
 
     def sf_autoarima_wiggly_mean_d05_m5(y: Y_TYPE, s: dict, k: int, a: A_TYPE = None, t: T_TYPE = None,
