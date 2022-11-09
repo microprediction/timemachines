14ffffb291bdc15b8a1428bed070540064e78bd6
Peter Cotton <peter.cotton@microprediction.com>
2022-11-09 13:07:55 -0500

    inclusion error

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -1,4 +1,5 @@
-from timemachines.skatertools.tuning.hyper import using_humpday
+from timemachines.inclusion.humpdayinclusion import using_humpday
+
 if using_humpday:
     from humpday.optimizers.alloptimizers import OPTIMIZERS
     from humpday.objectives.classic import CLASSIC_OBJECTIVES
@@ -12,6 +13,9 @@ if using_humpday:
         optimizer = random.choice(OPTIMIZERS)
         objective = random.choice(CLASSIC_OBJECTIVES)
         best_val, best_x, feval_count = optimizer(objective, n_trials=n_trials,n_dim=n_dim,with_count=True)
+else:
+    def test_optimizers():
+        print('pip install humpday')
 
 
 if __name__=='__main__':
