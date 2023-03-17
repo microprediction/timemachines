4d4b5dfb80cf0a75e1d2f5ef54c20c85fae90895
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2023-03-17 05:22:08 -1000

    Update test_tsa_ensemble.py

--- tests/tsa/test_tsa_ensemble.py
+++ tests/tsa/test_tsa_ensemble.py
@@ -1,5 +1,6 @@
 
 from timemachines.inclusion.statsmodelsinclusion import using_statsmodels
+from timemachines.inclusion.sklearninclusion import using_sklearn
 import numpy as np
 
 if using_statsmodels and using_sklearn:
